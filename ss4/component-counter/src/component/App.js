5c32dc0f26ccf000c774b9b58f0b77e1eee80551
Admin <vuthanhlong0906438630@gmail.com>
2023-07-24 10:50:49 +0700

    Message

--- ss4/component-counter/src/component/App.js
+++ ss4/component-counter/src/component/App.js
@@ -1,54 +1,29 @@
-import React, { Component } from "react";
-import './App.css';
 
-class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      list: [],
-      item: ""
-    }
+import React, { useState } from 'react'
 
-  };
+function useCounter(firstCount, step) {
+  const [count, setCount] = useState(firstCount)
 
-
-  handleChange = (event) => {
-    this.setState({ item: event.target.value })
+  const increment = () => {
+    setCount((count) => count + step)
   }
 
-  handleAddItem = () => {
-    if (this.state.item !== "") {
-      const newList = [...this.state.list, this.state.item]
-      this.setState(() => ({ list: newList, item: "" }))
-    }
-  }
+  return [count, increment]
+}
 
-  render() {
-    return (
-      <>
-        <div style={{ textAlign: 'center' }}>
-          <h1 >Todo List</h1>
-          <input type='text' value={this.state.item} onChange={this.handleChange} />
-          <button onClick={this.handleAddItem}>Add</button>
-          <table>
-            <thead>
-              <tr>
-                <th>Todo</th>
-              </tr>
-            </thead>
-            <tbody>{
-              this.state.list.map((todo, index) => (
-                <tr key={index}>
-                  <td>{todo}</td>
-                </tr>
-              ))}
-            </tbody>
-          </table>
-        </div>
-      </>
-    );
-  }
-
+function App() {
+  const [count1, setCount1] = useCounter(0, 1);
+  const [count2, setCount2] = useCounter(0, 2);
+  return (
+    <div style={{ textAlign: 'center' }}>
+      <button onClick={setCount1}
+      >Add 1</button>
+      <h1>Count: {count1}</h1>
+      <button onClick={setCount2}
+      >Add 2</button>
+      <h1>Count: {count2}</h1>
+    </div>
+  );
 }
 
 export default App;

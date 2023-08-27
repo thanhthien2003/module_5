fa8d21fea2aace6dcf44d9d677c16d8e568800cb
Admin <vuthanhlong0906438630@gmail.com>
2023-08-27 17:08:37 +0700

    Message

---
+++ case_study/case-study/src/components/ListContact.js
@@ -0,0 +1,31 @@
+import React from "react";
+import "bootstrap/dist/css/bootstrap.css";
+
+export function ListContact() {
+    return(
+        <>
+        <table className="table table-bordered">
+        <thead>
+        <tr>
+        <th>STT</th>
+        <th>Contract number</th>
+        <th>Day start</th>
+        <th>Day end</th>
+        <th>Down Payment</th>
+        <th>Total amount</th>
+    </tr>
+        </thead>
+        <tbody>
+        <tr>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+    </tr>
+        </tbody>
+      </table>
+        </>
+    )
+}
--- case_study/case-study/src/index.js
+++ case_study/case-study/src/index.js
@@ -2,22 +2,23 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import "bootstrap/dist/css/bootstrap.css";
-import EditService from './components/editService';
-import ListService from './components/listService';
+import EditService from './components/EditService';
+import ListService from './components/ListService';
 import './css/editService.css'
 import './css/createService.css'
-import CreateService from './components/createService';
+import CreateService from './components/CreateService';
 import '@fortawesome/fontawesome-free/css/all.min.css';
 import './css/service.css'
 import './css/createCustomer.css'
 import './css/editCustomer.css'
-import { ListCustomer } from './components/listCustomer';
-import { CreateCustomer } from './components/createCustomer';
-import { EditCustomer } from './components/editCustomer';
+import { ListCustomer } from './components/ListCustomer';
+import { CreateCustomer } from './components/CreateCustomer';
+import { EditCustomer } from './components/EditCustomer';
+import CreateContact from './components/CreateContact';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-   <EditCustomer />
+   <CreateContact />
 );
 
